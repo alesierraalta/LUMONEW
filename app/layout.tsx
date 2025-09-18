f9443e9d50d55281c8399f9ebbe24fe604e24fb7
alesierraalta <alejandrosa2000@gmail.com>
2025-09-18 09:23:55 -0400

    speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -30,6 +31,7 @@ export default function RootLayout({
     <html suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   )
